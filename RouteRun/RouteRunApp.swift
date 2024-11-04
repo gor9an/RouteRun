887d25c0c141507e0a6ce1cfcaa35bdc598c6b84
gor9an <gordanandrey@yandex.ru>
2024-11-04 13:45:53 +0300

    Initial Commit

---
+++ RouteRun/RouteRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteRunApp.swift
+//  RouteRun
+//
+//  Created by Andrey Gordienko on 04.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct RouteRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
